09bf0734a8ceee2fea23230c3ee2af08a5cd05e7
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-09 12:50:06 +0530

    Implement string repeat function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(message, times) {
+    if(times < 0) return 'ERROR';
+    let output = '';
+    for (i = 1; i <= times ; i++) {
+        output += message;
+    }
+    return output;
 };
 
 // Do not edit below this line
