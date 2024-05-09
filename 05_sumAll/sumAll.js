17d6cb42d061e867422b1c15c46efff0e82fc7aa
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-09 16:36:28 +0530

    Create a function to sum integers within a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+        return 'ERROR';
+    }
 
+    // code to make num1 the smaller number among the two
+    if (num1 > num2) {
+        let temp;
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    let sum = 0;
+
+    for (let num = num1; num <= num2; num++) {
+        sum += num;
+    }
+    return sum;
 };
 
 // Do not edit below this line
