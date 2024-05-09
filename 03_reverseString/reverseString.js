de15e886daf8fe29471066e5bfab8f0b7ded0d66
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-09 13:12:17 +0530

    Create a function for string reversal

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(inputString = '') {
+    // In JavaScript, since strings are immutable. Convert to array, reverse, then join.
+    return inputString.split('').reverse().join('');
 };
 
 // Do not edit below this line
