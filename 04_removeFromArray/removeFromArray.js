fb445a77007572b58fab20cff8b90d30d8170d45
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-09 16:16:11 +0530

    Create function to remove arguments from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, ...args) {
+    for(let i = 0; i < args.length; i++) {
+        givenArray = givenArray.filter(function(item){return item!==args[i];});
+    }
+    return givenArray;
 };
 
 // Do not edit below this line
