4422d49617d37f0863a1889986591db903a4358e
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-18 01:04:58 +0530

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let checker='';
+    for(i=0; i<str.length; i++){
+        let ascii = str.charCodeAt(i);
+        if(ascii>=65 && ascii<=90 || ascii>=97 && ascii<=122 || ascii>=48 && ascii<=57) {
+            checker += str.charAt(i);
+        }
+    }
+    checker = checker.toLowerCase();
+    let reversedChecker = checker.split('').reverse().join('');
 
+    return (checker === reversedChecker);
 };
 
 // Do not edit below this line
