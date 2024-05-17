565a2a0a80f74689771a22c498b9118af49ade9f
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-18 01:24:54 +0530

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    position = parseInt(position);
+    if(position<0 || typeof position !== 'number') return 'OOPS';
+    if(position===0) return 0;
+    if(position===1 || position===2) return 1;
 
+    return fibonacci(position - 1) + fibonacci(position-2);
 };
 
 // Do not edit below this line
