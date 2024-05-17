6577d764cc83219630e1af815e5891a67ac23bd5
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-18 02:09:49 +0530

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.forEach(function (person){
+        if(person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
 
+    people.sort((a,b)=>((b.yearOfDeath - b.yearOfBirth)-(a.yearOfDeath - a.yearOfBirth)));
+    return people[0];
 };
 
 // Do not edit below this line
